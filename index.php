6d5b7707111daa7e01a583d0250649d306a3e8b6
elinsvante <elin.svantesson@outlook.com>
2018-05-08 16:07:36 +0200

    Börjat med headern

--- index.php
+++ index.php
@@ -5,11 +5,18 @@
     <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>Hälsocoachen</title>
+    <link rel = "stylesheet" href = "main.css">
     </head>
         <body>
 <div id = "header">
 
 <h>Hälsocoachen</h1>
+
+ <ul>
+  <li><a href="default.asp">Vad är hälsocoaching?</a></li>
+  <li><a href="news.asp">Om oss</a></li>
+  <li><a href="contact.asp">Logga In/Registrera</a></li>
+</ul> 
 
 </div>
 
