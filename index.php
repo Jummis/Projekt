c67932759df3f257d3abc93e64fa98fd873562b1
elinsvante <elin.svantesson@outlook.com>
2018-05-08 15:37:14 +0200

    Skapade en header

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
+<!DOCTYPE>
+
 <html>
 
     <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>Hälsocoachen</title>
     </head>
-
         <body>
+<div id = "header">
+
+<h>Hälsocoachen</h1>
+
+</div>
+
         </body>
 
 </html>
